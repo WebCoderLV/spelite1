573a8c3ac6db8562713e4532f98297c0e33136c9
Rutaer <arturser@yahoo.com>
2025-10-21 13:45:37 +0300

    Validation strādā!

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { Field, form } from '@angular/forms/signals';
+import { Field, form, maxLength, minLength, required } from '@angular/forms/signals';
 import { UserInterface } from '../models/user-interface';
 
 @Component({
@@ -15,10 +15,20 @@ export class Login {
     password: '',
   });
 
-  protected readonly loginForm = form(this.user);
+  protected readonly loginForm = form(this.user, (path) => {
+    required(path.username, { message: 'Username is required' });
+    minLength(path.username, 3, { message: 'Username must be at least 3 characters' });
+    maxLength(path.username, 25, { message: 'Username cannot exceed 25 characters' });
+
+    required(path.password, { message: 'Password is required' });
+    minLength(path.password, 4, { message: 'Password must be at least 4 characters' });
+    maxLength(path.password, 50, { message: 'Password cannot exceed 50 characters' });
+  });
 
   onLogIn() {
-    console.log('User ', this.user(), ' logged in');
+    if (this.loginForm().valid()) {
+      console.log('User ', this.user(), ' logged in');
+    }
   }
 
 }
